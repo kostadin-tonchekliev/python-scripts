86c6a2ac1a0a8dc8ad0e85e4fd0cf1689d61dd7e
Kostadin Tonchekliev <kostadintonchekliev@Kostadins-MacBook-Pro.local>
2022-02-15 15:59:16 +0200

    Added loop functionality

--- SSH-connection-manager/sshcon.py
+++ SSH-connection-manager/sshcon.py
@@ -1,12 +1,11 @@
 #!/usr/bin/env python3
 
 import json
-import os
 import subprocess
 from simple_term_menu import TerminalMenu
 from pathlib import Path
 
-print("SSH Connection Manager ver 4.0")
+print("SSH Connection Manager ver 5.0")
 
 #Main path to the json file
 json_path = "/Users/kostadintonchekliev/Desktop/scripts/Python/SSH_Connection/conn_data.json"
@@ -14,44 +13,63 @@ json_path = "/Users/kostadintonchekliev/Desktop/scripts/Python/SSH_Connection/co
 menu_options = []
 
 #Functions
+def json_check():
+    if not Path(json_path).exists():
+        print("File doesn't exist, creating it for you")
+        Path(json_path).touch()
 def return_key(option):
     return list(conn_dict.keys())[option]
 def return_value(option):
     return list(conn_dict.values())[option]
+def read_data():
+    with open(json_path, 'r') as json_data:
+        try:
+            main_dict = json.load(json_data)
+            for result in main_dict.keys():
+                menu_options.append(result)
+        except:
+            main_dict = {}
+
+        return main_dict
 def update_data():
     with open(json_path, 'w') as json_file:
         json.dump(conn_dict, json_file)
 
-#Check if the json file exist
-if not Path(json_path).exists():
-    print("File doesn't exist, creating it for you")
-    Path(json_path).touch()
-
-with open(json_path, 'r') as json_data:
-    try:
-        conn_dict = json.load(json_data)
-        for result in conn_dict.keys():
-            menu_options.append(result)
-    except:
-        conn_dict = {}
-
-if len(menu_options) > 0:
-    menu_options.extend(("Remove Connections", "Add New", "Exit"))
-else:
-    menu_options.extend(("Add New", "Exit"))
-
-choice = TerminalMenu(menu_options, title="Please select an action:").show()
-if choice == len(menu_options) - 1:
-    exit("Exiting script..")
-elif choice == len(menu_options) - 2:
+def add_conn():
     label = input("Please select label: ")
     username = input("Please enter username: ")
     hostname = input("Please enter hostname: ")
     conn_dict[label] = f"ssh {username}@{hostname} -p18765"
     update_data()
-elif choice == len(menu_options) - 3:
-    choice2 = TerminalMenu(menu_options[:-3], title="Please select which connection to remove:").show()
-    del conn_dict[return_key(choice2)]
-    update_data()
-else:
-    subprocess.run(return_value(choice), shell=True)
+
+def remove_conn():
+    choice2 = TerminalMenu(menu_options[:-3] + ["Exit"], title="Please select which connection to remove:").show()
+    if choice2 == len(menu_options) - 3:
+        print("Returning")
+    else:
+        del conn_dict[return_key(choice2)]
+        update_data()
+def build_menu():
+    if len(menu_options) > 0:
+        menu_options.extend(("Remove Connections", "Add New", "Exit"))
+    elif len(menu_options) == 0:
+        menu_options.extend(("Add New", "Exit"))
+
+if __name__ == '__main__':
+    json_check()
+
+    while True:
+        conn_dict = read_data()
+        build_menu()
+        choice = TerminalMenu(menu_options, title="Please select an action:").show()
+        if choice == len(menu_options) - 1:
+            exit("Exiting script. Goodbye")
+        elif choice == len(menu_options) - 2:
+            add_conn()
+        elif choice == len(menu_options) - 3:
+            remove_conn()
+        else:
+            subprocess.run(return_value(choice), shell=True)
+            exit()
+
+        menu_options = []
