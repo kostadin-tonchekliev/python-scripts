954e9a4dfe85652c444a42a3f6426f2c92d350e5
Kostadin Tonchekliev <kostadintonchekliev@Kostadins-MacBook-Pro.local>
2022-02-25 16:17:32 +0200

    Added scraper script

---
+++ emag-scraper/README.md
@@ -0,0 +1,40 @@
+Web scraper designed to work with emag.bg. Current features: <br>
+* monitor the price of different products and show if it drops or is increased
+* show if a product is out of stock and shows if it becomes available
+* support running the script with [xbar](https://xbarapp.com/) for showing the data in your taskbar
+---
+### Requirements
+You need to have the following python packages installed:
+```commandline
+pip3 install simple_term_menu
+pip3 install pathlib
+pip3 install bs4
+pip3 install requests
+pip3 install json
+pip3 install sys
+```
+
+In the begging of the file update `json_path` to the full desired path where you want to have the main json file stored.
+
+### Usage
+-Run the file without any arguments for xbar mode (this means that it will display only the end data): <br>
+```
+Koce@ ~/Python/Web-Scraper:py scrape-main.py
+View Scrape
+---
+LG-Cheap: 695.04 =
+32UltraWide: 799.99 =
+274K-The one: 727.92 =
+TheOne: 1102.74 =
+StockTest: Not in stock
+```
+
+-Run the file with the argument `-u` for full mode where you can add new connections, remove existing ones and etc. : <br>
+```
+Koce@ ~/Python/Web-Scraper:py scrape-main.py -u
+Please select:
+> Add new product
+  Remove existing product
+  Show all products
+  Exit
+```
---
+++ emag-scraper/scrape-main.py
@@ -0,0 +1,163 @@
+#!/usr/bin/env PYTHONIOENCODING=UTF-8 /usr/local/bin/python3
+
+import json
+import sys
+from pathlib import Path
+from simple_term_menu import TerminalMenu
+from bs4 import BeautifulSoup
+import requests
+
+json_path = '/example/dir'
+
+def emag_check():
+    try:
+        requests.get('https://www.emag.bg/')
+    except:
+        print("Error reaching Emag server")
+        exit()
+
+
+def json_check():
+    if not Path(f'{json_path}products.json').exists() == True:
+        print("File doesn't exists, created it for you")
+        Path(f'{json_path}products.json').touch()
+
+
+def read_json():
+    global products_list
+    with open(f'{json_path}products.json', 'r') as json_data:
+        try:
+            products_list = json.load(json_data)
+        except:
+            products_list = {}
+
+
+def update_json():
+    with open(f'{json_path}products.json', 'w') as json_data:
+        json.dump(products_list, json_data)
+
+
+def build_menu():
+    global menu_options
+    menu_options = []
+    if len(products_list.keys()) > 0:
+        menu_options.extend(("Add new product", "Remove existing product", "Show all products", "Exit"))
+    else:
+        menu_options.extend(("Add new product", "Exit"))
+
+
+def read_menu(listt):
+    choice = TerminalMenu(listt, title="Please select:").show()
+    return choice
+
+
+def return_action(dictt, selection):
+    return dictt[selection]
+
+
+def data_scrape(link):
+    page_content = requests.get(link)
+    soup = BeautifulSoup(page_content.content, 'lxml')
+    title = soup.find('h1', class_='page-title').text.strip().split(',')[0]
+    price = soup.find('p', class_='product-new-price').text.strip()
+    try:
+        soup.find('span', class_='label label-out_of_stock').text.strip()
+        price = 0
+    except:
+        price = beautify(price)
+    return title, price
+
+
+def add_data():
+    label = input("Please enter label:")
+    while label in products_list:
+        label = input("Label already exists, please enter a new one: ")
+    url = input("Please enter url:")
+    try:
+        requests.get(url)
+    except:
+        print("Incorrect or broken link, exiting without adding data")
+        exit()
+    product_title, product_price = data_scrape(url)
+    products_list[label] = product_title, product_price, url
+
+
+def remove_data():
+    choice = read_menu(list(products_list.keys()) + ["Exit"])
+    if choice == len(products_list.keys()):
+        exit()
+    else:
+        del products_list[list(products_list)[choice]]
+
+
+def beautify(original_price):
+    new_price = ""
+    original_price = original_price.replace('.', '')
+    original_price = list(original_price.split()[0])
+    original_price.insert(-2, '.')
+    for char in original_price:
+        new_price = new_price + str(char)
+    return float(new_price)
+
+
+def main_actions():
+    if action == "Add new product":
+        add_data()
+        update_json()
+    elif action == "Remove existing product":
+        remove_data()
+        update_json()
+    elif action == "Show all products":
+        for object in products_list:
+            print(
+                f'Label: {object}, Name: {products_list[object][0]}, Price: {products_list[object][1]}, URL: {products_list[object][2]}')
+        exit()
+    elif action == "Exit":
+        exit()
+
+
+def check_argument():
+    try:
+        option = sys.argv[1]
+    except IndexError:
+        option = 'none'
+    return option
+
+
+def show_prices_compared():
+    for product in products_list:
+        new_price = data_scrape(products_list[product][2])[1]
+        if new_price == 0:
+            print(f"{product}: Not in stock")
+        elif new_price == products_list[product][1]:
+            print(f"{product}: {new_price} =")
+        elif new_price > products_list[product][1]:
+            print(f"{product}: {new_price} \u1403")
+        elif new_price < products_list[product][1]:
+            print(f"{product}: {new_price} \u1401")
+
+def xbar_show():
+    if len(products_list.keys()) > 0:
+        print("View Scrape")
+        print("---")
+        show_prices_compared()
+    else:
+        print("No products")
+
+
+if __name__ == '__main__':
+    in_option = check_argument()
+
+    if in_option == '-u':
+        json_check()
+        while True:
+            read_json()
+            build_menu()
+            action = return_action(menu_options, read_menu(menu_options))
+            main_actions()
+    elif in_option == 'none':
+        emag_check()
+        read_json()
+        xbar_show()
+    else:
+        print("Wrong option selected, currently works only with '-u' and empty")
