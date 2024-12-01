8309fccf2c7dcfc5cc89ca52f82c33f3ce5dcd4c
kostadin-tonchekliev <kostadintonchekliev@gmail.com>
2024-12-01 14:15:42 +0200

    Added advent-of-code-2024

---
+++ advent-of-code-2024/README.md
@@ -0,0 +1,36 @@
+## Executor for the Advent of Code 2024 challenges
+
+---
+
+This script is designed to dynamically combine all Advent of Code 2024 challenges into a single executable and run specific days.
+
+### Setup:
+1. Clone the folder
+2. Initialize a python virtual environment using the following command:
+```shell
+python3 -m venv shadowRealm
+```
+3. Enter the virtual environment
+```shell
+source shadowRealm/bin/activate
+```
+4. Install the requirements
+```shell
+python3 -m pip install -r requirements.txt
+```
+
+### Usage:
+Run the `main.py` executable:
+```shell
+python3 main.py
+```
+
+By default it will run the `main` function of the selected day, however you can also provide a specific day to be executed and which function to run. This is an example which runs day 2 and the test function:
+```shell
+python3 main.py 2 test
+```
+
+### Configuration:
+- Days should be added into the `days` folder, each day needs to be separated in a different folder
+- Days should all include a `main.py` file
+- The `main.py` file needs to include a `main()` function which will be run by default, additional functions currently supported: `test`. Any additional functions need to be defined in the `main.py` file as well (in the switch case on line **76**)
---
+++ advent-of-code-2024/main.py
@@ -0,0 +1,80 @@
+import os
+import importlib.util
+import sys
+import string
+import secrets
+from simple_term_menu import TerminalMenu
+
+
+def load_module(source):
+    """
+    reads file source and loads it as a module
+    credit to: https://medium.com/@david.bonn.2010/dynamic-loading-of-python-code-2617c04e5f3f
+
+    :param source: file to load
+    :return: loaded module
+    """
+
+    module_name = 'gensym_' + "".join([secrets.choice(string.ascii_uppercase + string.ascii_lowercase + string.digits) for i in range(32)])
+
+    spec = importlib.util.spec_from_file_location(module_name, source)
+    module = importlib.util.module_from_spec(spec)
+    sys.modules[module_name] = module
+    spec.loader.exec_module(module)
+
+    return module
+
+
+def getDirectories() -> list:
+    """
+    list the available days in the `days` folder
+
+    :return: list of days
+    """
+
+    availableDays = []
+
+    for singleDir in os.listdir('days'):
+        if os.path.isdir(f"days/{singleDir}"):
+            availableDays.append(singleDir)
+
+    return availableDays
+
+
+if __name__ == '__main__':
+    days = getDirectories()
+    action = 'main'
+    args = sys.argv
+
+    if len(days) == 0:
+        print("[Info] Unable to find any days to load")
+        exit(0)
+
+    match len(args):
+        case 1:
+            menuObject = TerminalMenu(days, title='Select day:')
+            dayChoice = days[menuObject.show()]
+        case 2:
+            if args[1] in days:
+                dayChoice = args[1]
+            else:
+                print(f"[Err] Invalid day chosen: {args[1]}")
+                exit(1)
+        case 3:
+            if args[1] in days:
+                dayChoice = args[1]
+                action = args[2]
+            else:
+                print(f"[Err] Invalid day chosen: {args[1]}")
+                exit(1)
+        case _:
+            print("[Err] Invalid number of arguments provided!")
+            exit(1)
+
+    customModule = load_module(f'days/{dayChoice}/main.py')
+
+    match action:
+        case 'main':
+            customModule.main()
+        case 'test':
+            customModule.test()
